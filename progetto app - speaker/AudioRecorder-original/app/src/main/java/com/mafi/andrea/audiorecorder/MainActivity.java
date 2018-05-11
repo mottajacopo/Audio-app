a627989cccdb6f36733bc5d9d5dd5435883f8179
mottajacopo <mottajacopo@gmail.com>
2018-05-11 11:17:32 +0200

    aggiunta app AudioRecorder-original quella fatta in aula senza nessuna libreria aggiunta chi la vuole usare ne faccia una copia

---
+++ progetto app - speaker/AudioRecorder-original/app/src/main/java/com/mafi/andrea/audiorecorder/MainActivity.java
@@ -0,0 +1,33 @@
+package com.mafi.andrea.audiorecorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnRec = null;
+
+    private final String PATH = "andre_records";
+    private final String FILENAME = "rec.wav";
+
+    private final int Fs = 44200;
+    private final int recordingLength = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRec = findViewById(R.id.btn);
+        btnRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Rec rec = new Rec(getApplicationContext(), recordingLength, Fs);
+                rec.execute(PATH, FILENAME);
+            }
+        });
+    }
+}
