3c24dd0e5c1242f1275bb867a39541be7292b01e
mottajacopo <mottajacopo@gmail.com>
2018-05-12 19:07:54 +0200

    Modifica SpeechToText_IBM

--- progetto app - speech/SpeechToText_IBM/app/src/main/java/com/example/motta/speechtotextv3/SpeechToTextExample.java
+++ progetto app - speech/SpeechToText_IBM/app/src/main/java/com/example/motta/speechtotextv3/SpeechToTextExample.java
@@ -1,56 +1,41 @@
 package com.example.motta.speechtotextv3;
 
-/**
- * Copyright 2017 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
-import android.nfc.Tag;
-import android.util.Log;
-
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import com.ibm.watson.developer_cloud.http.HttpMediaType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeCallback;
 
-/**
- * Recognize a sample wav file and print the transcript into the console output. Make sure you are using UTF-8 to print
- * messages; otherwise, you will see question marks.
- */
 public class SpeechToTextExample {
+    private static CountDownLatch lock = new CountDownLatch(1);
 
-    private final static String TAG = "Rec";
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         SpeechToText service = new SpeechToText();
         service.setUsernameAndPassword("<username>", "<password>");
 
-        try{
-            File audio = new File("src/test/resources/speech_to_text/sample1.wav");
+        FileInputStream audio = new FileInputStream("src/test/resources/speech_to_text/sample1.wav");
 
-            RecognizeOptions options = new RecognizeOptions.Builder()
-                    .audio(audio)
-                    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
-                    .build();
-            SpeechRecognitionResults transcript = service.recognize(options).execute();
+        RecognizeOptions options = new RecognizeOptions.Builder()
+                .audio(audio)
+                .interimResults(true)
+                .contentType(HttpMediaType.AUDIO_WAV)
+                .build();
 
-            System.out.println(transcript);
+        service.recognizeUsingWebSocket(options, new BaseRecognizeCallback() {
+            @Override
+            public void onTranscription(SpeechRecognitionResults speechResults) {
+                System.out.println(speechResults);
 
-        }
-        catch (FileNotFoundException e){
-            Log.e(TAG,"File not found");
-        }
-
-
+            }
+            @Override
+            public void onDisconnected() {
+                lock.countDown();
+            }
+        });
+        lock.await(1, TimeUnit.MINUTES);
     }
 }
