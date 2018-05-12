d2d14c47eef08e9a5ca0fdc784f89f6f7a1e1a3f
mottajacopo <mottajacopo@gmail.com>
2018-05-12 12:09:40 +0200

    aggiunta versione SpeechToText_IBM_web

---
+++ progetto app - speech/SpeechToText_IBM_web/app/src/main/java/com/example/motta/speechtotextv3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.motta.speechtotextv3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private Button btt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+        btt = (Button) findViewById(R.id.btt);
+
+        btt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new SpeechToTextExample();
+            }
+        });
+
+    }
+}
---
+++ progetto app - speech/SpeechToText_IBM_web/app/src/main/java/com/example/motta/speechtotextv3/SpeechToTextExample.java
@@ -0,0 +1,79 @@
+package com.example.motta.speechtotextv3;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.ibm.watson.developer_cloud.http.HttpMediaType;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeCallback;
+
+public class SpeechToTextExample {
+    private static CountDownLatch lock = new CountDownLatch(1);
+
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        SpeechToText service = new SpeechToText();
+        service.setUsernameAndPassword("<username>", "<password>");
+
+        FileInputStream audio = new FileInputStream("src/test/resources/speech_to_text/sample1.wav");
+
+        RecognizeOptions options = new RecognizeOptions.Builder()
+                .audio(audio)
+                .interimResults(true)
+                .contentType(HttpMediaType.AUDIO_WAV)
+                .build();
+
+        service.recognizeUsingWebSocket(options, new BaseRecognizeCallback() {
+            @Override
+            public void onTranscription(SpeechRecognitionResults speechResults) {
+                System.out.println(speechResults);
+            }
+            @Override
+            public void onDisconnected() {
+                lock.countDown();
+            }
+        });
+        lock.await(1, TimeUnit.MINUTES);
+    }
+}
+
+/*
+public class SpeechToTextExample {
+
+    private final static String TAG = "Rec";
+
+    public static void main(String[] args) {
+
+        SpeechToText service = new SpeechToText();
+        service.setUsernameAndPassword("<speech_text_username>", "<speech_text_password>");
+
+        try{
+            InputStream audio = new FileInputStream("src/test/resources/sample1.wav");
+
+            RecognizeOptions options = new RecognizeOptions.Builder()
+                    .audio(audio)
+                    .contentType(HttpMediaType.AUDIO_WAV)
+                    .interimResults(true)
+                    .build();
+
+            service.recognizeUsingWebSocket(options, new BaseRecognizeCallback() {
+                @Override
+                public void onTranscription(SpeechRecognitionResults speechResults) {
+                    System.out.println(speechResults);
+                }
+            });
+        }
+        catch (FileNotFoundException e){
+            Log.e(TAG,"File not found");
+        }
+    }
+}
+*/
