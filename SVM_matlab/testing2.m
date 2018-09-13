5ef19a145b55935aded1eea43c6c9eb728ad7a85
mottajacopo <mottajacopo@gmail.com>
2018-09-13 14:12:03 +0200

    huvdvuv

--- SVM_matlab/testing2.m
+++ SVM_matlab/testing2.m
@@ -1,30 +1,43 @@
 function testing2(model,normValsOut)
 
-    folder = 'frase3';
-
-    for i=1:1:10    
-    file = strcat('MJ',int2str(i),'.txt');
-    read_predict (model , file , folder , 299 ,normValsOut);
-    end
+%     folder = 'frase3';
+% 
+%     for i=1:1:10    
+%     file = strcat('MJ',int2str(i),'.txt');
+%     read_predict (model , file , folder , 299 ,normValsOut);
+%     end
+%     
+%     for i=1:1:10 
+%     file = strcat('MB',int2str(i),'.txt');
+%     read_predict (model , file , folder , 299,normValsOut);
+%     end
+%     
+%     for i=1:1:10   
+%     file = strcat('MT',int2str(i),'.txt');
+%     read_predict (model , file , folder , 299,normValsOut);
+%     end
+%     
+%     for i=1:1:10
+%     file = strcat('CC',int2str(i),'.txt');
+%     read_predict (model , file , folder , 299,normValsOut);
+%     end
+%     
+%     folder = 'frase6';
+%     for i=1:1:20 
+%     file = strcat('testDataFormat',int2str(i),'.txt');
+%     read_predict (model , file , folder , 299,normValsOut);
+%     end
     
-    for i=1:1:10 
-    file = strcat('MB',int2str(i),'.txt');
-    read_predict (model , file , folder , 299,normValsOut);
-    end
     
-    for i=1:1:10   
-    file = strcat('MT',int2str(i),'.txt');
-    read_predict (model , file , folder , 299,normValsOut);
-    end
-    
+    folder = 'rec no mic\MJ\0.02';
     for i=1:1:10
-    file = strcat('CC',int2str(i),'.txt');
+    file = strcat('MJ',int2str(i),'.txt');
     read_predict (model , file , folder , 299,normValsOut);
     end
     
-    folder = 'frase6';
-    for i=1:1:20 
-    file = strcat('testDataFormat',int2str(i),'.txt');
+    folder = 'rec no mic\MB\0.02';
+    for i=1:1:10
+    file = strcat('MB',int2str(i),'.txt');
     read_predict (model , file , folder , 299,normValsOut);
     end
 
