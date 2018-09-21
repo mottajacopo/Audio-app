89b8fe26cac543948540b5b2a48f8cfb4cd64718
marcoballetto <marco.balletto1gmail.com>
2018-09-21 22:13:29 +0200

    txt generator

--- RecorderSpeakerSpeech/app/src/main/java/com/example/motta/recorderspeakerspeech/MainActivity.java
+++ RecorderSpeakerSpeech/app/src/main/java/com/example/motta/recorderspeakerspeech/MainActivity.java
@@ -4,20 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnRec = null;
-    Button bttStt = null;
+    EditText editText = null;
     Button bttSpk = null;
     TextView textView = null;//textView per stampare i messaggi di risultato
 
     private final String PATH = "Audio recognition files multi";//nome cartella contenente tutti i file per speaker e speech recog.
     private final String FILENAME = "rec.wav";//nome file .wav
 
-    private final int Fs = 8000;//frequenza di campionamento
+    private final int Fs = 44000;//frequenza di campionamento
     private final int recordingLength = 3;//lunghezza registraz. in secondi
 
 
@@ -26,44 +27,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRec = findViewById(R.id.bttRec);
         bttSpk = findViewById(R.id.bttSpk);
-        bttStt = findViewById(R.id.bttStt);
         textView = findViewById(R.id.textView);
-
-
-        btnRec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                Rec rec = new Rec(getApplicationContext(), recordingLength, Fs);
-                rec.execute(PATH, FILENAME);
-
-            }
-        });
+        editText = findViewById(R.id.editText);
 
 
         bttSpk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                SpeakerRecog speakerRecog = new SpeakerRecog(getApplicationContext(),Fs,recordingLength,textView);
-                speakerRecog.execute(PATH,FILENAME);
+                SpeakerRecog speakerRecog = null;
+
+                String numberOfWavs = editText.getText().toString();
+
+                for(int i = 1; i< Integer.valueOf(numberOfWavs) + 1; i++) {
+
+                    speakerRecog = new SpeakerRecog(getApplicationContext(),Fs,recordingLength,textView);
+                    speakerRecog.execute(PATH, String.valueOf(i),numberOfWavs);
+                }
 
             }
         });
 
-
-        bttStt.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                STT stt = new STT(getApplicationContext(),textView);
-                stt.execute(PATH, FILENAME);
-            }
-        });
 
 
     }
--- RecorderSpeakerSpeech/app/src/main/java/com/example/motta/recorderspeakerspeech/SpeakerRecog.java
+++ RecorderSpeakerSpeech/app/src/main/java/com/example/motta/recorderspeakerspeech/SpeakerRecog.java
@@ -24,12 +24,13 @@ import libsvm.svm_node;
 import static com.example.motta.recorderspeakerspeech.SupportFunctions.computeDeltas;
 import static com.example.motta.recorderspeakerspeech.SupportFunctions.convertFloatsToDoubles;
 import static com.example.motta.recorderspeakerspeech.SupportFunctions.matlabModelToAndroidModel;
+import static com.example.motta.recorderspeakerspeech.SupportFunctions.printFeaturesOnFileFormat;
 import static com.example.motta.recorderspeakerspeech.SupportFunctions.readTestDataFromFormatFile;
 import static com.example.motta.recorderspeakerspeech.SupportFunctions.scaleTestData;
 import static com.example.motta.recorderspeakerspeech.SupportFunctions.uniteAllFeaturesInOneList;
 
 
-public class SpeakerRecog extends AsyncTask<String,Void,String> {
+public class SpeakerRecog extends AsyncTask<String,Void,Boolean> {
 
     private final double frameLength = 0.02;// lunghezza frame in secondi
     private final double overlapPercentage = 0.5;//percentuale sovrapp. dei frame
@@ -40,7 +41,6 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
     private int recordingLengthInSec = 0;
     private int nSamples = 0;
     private int nSamplesPerFrame = 0;
-    private TextView textView = null;
 
     public SpeakerRecog(Context _context, int _Fs, int _recordingLengthInSec, TextView _textView)
     {
@@ -53,7 +53,6 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
 
         samples = new short[nSamples];
 
-        textView = _textView;
     }
 
 
@@ -61,14 +60,23 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
     protected void onPreExecute() {
         super.onPreExecute();
 
-        Toast.makeText(context,"Starting recognition",Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected String doInBackground(String... strings) {
+    protected Boolean doInBackground(String... strings) {
 
         String path = Environment.getExternalStorageDirectory() + "/" + strings[0];
-        String filePath = path + "/" + strings[1];
+        String recNumber = strings[1];
+        String filePath = path + "/" + "rec" + recNumber + ".wav";
+        String numberOfWavFiles = strings[2];
+
+        boolean doToast = false;
+
+        if(Integer.valueOf(numberOfWavFiles) == Integer.valueOf(recNumber))
+        {
+            doToast = true;
+        }
+
 
         WavIO readWav = new WavIO(filePath);
 
@@ -80,26 +88,25 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
                 throw new IOException("Error while reading .wav file");
             }
 
-        }
-        catch (IOException exception){
-            Log.e("Speaker Recognition",exception.getMessage());
+        } catch (IOException exception) {
+            Log.e("Speaker Recognition", exception.getMessage());
             return null;//se è stata generata l'eccezione esco dal doInBackground
         }
 
 
         short value = 0;
 
-        for(int i = 0;  i < nSamples; i++)//ricostruisco i campioni originali a partire dai byte salvati nel file .wav
+        for (int i = 0; i < nSamples; i++)//ricostruisco i campioni originali a partire dai byte salvati nel file .wav
         {
 
-            value = (short) (((short) readWav.myData[2*i] & 0x00ff) | ((short)readWav.myData[(2*i)+1]<<8 & 0xff00));//ricostruisco il campione a 16 bit unendo i primi 2 byte di myData
+            value = (short) (((short) readWav.myData[2 * i] & 0x00ff) | ((short) readWav.myData[(2 * i) + 1] << 8 & 0xff00));//ricostruisco il campione a 16 bit unendo i primi 2 byte di myData
 
             samples[i] = value;
         }
 
 
         int nSamplesAlreadyProcessed = 0;
-        int nSamplesOverlapped = (int)(overlapPercentage*nSamplesPerFrame);
+        int nSamplesOverlapped = (int) (overlapPercentage * nSamplesPerFrame);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //framing audio data
@@ -109,7 +116,7 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
 
         int samplesBack = 0;//per il primo frame (da 0 a nSamplesPerFrame campioni) non devo realizzare overlapping con il frame precedente
 
-        while (nSamples - nSamplesAlreadyProcessed >= nSamplesPerFrame-nSamplesOverlapped) { //dato l'overlapping per ogni frame avanzo di nSamplesPerFrame-nSamplesOverlapped frames
+        while (nSamples - nSamplesAlreadyProcessed >= nSamplesPerFrame - nSamplesOverlapped) { //dato l'overlapping per ogni frame avanzo di nSamplesPerFrame-nSamplesOverlapped frames
 
             float[] frame = new float[nSamplesPerFrame];
 
@@ -122,13 +129,12 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
 
             floatSamplesPerFrame.add(frame);
 
-            if(nSamplesAlreadyProcessed == 0) {//se sono al primo frame
+            if (nSamplesAlreadyProcessed == 0) {//se sono al primo frame
 
                 nSamplesAlreadyProcessed = nSamplesPerFrame;//i nuovi campioni considerati sono nSamplesPerFrame
                 samplesBack = nSamplesOverlapped;//i frame successivi al primo possono considerare nSamplesOverlapped campioni dal frame precedente
-            }
-            else {
-                nSamplesAlreadyProcessed += nSamplesPerFrame-nSamplesOverlapped;//negli altri casi i nuovi campioni sono nSamplesPerFrame-nSamplesOverlapped
+            } else {
+                nSamplesAlreadyProcessed += nSamplesPerFrame - nSamplesOverlapped;//negli altri casi i nuovi campioni sono nSamplesPerFrame-nSamplesOverlapped
             }
         }
 
@@ -139,11 +145,11 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
         ArrayList<double[]> deltadelta = new ArrayList<double[]>();
 
         /**//**//**//**/
-        TarsosDSPAudioFormat af = new TarsosDSPAudioFormat(Fs,16, 1,true,true);
+        TarsosDSPAudioFormat af = new TarsosDSPAudioFormat(Fs, 16, 1, true, true);
         AudioEvent ae = new AudioEvent(af);
-        MFCC mfcc = new MFCC(nSamplesPerFrame,Fs,13,30, 133.3334f, ((float)Fs)/2f);
+        MFCC mfcc = new MFCC(nSamplesPerFrame, Fs, 13, 30, 133.3334f, ((float) Fs) / 2f);
 
-        for(int j =0; j< floatSamplesPerFrame.size(); j++){
+        for (int j = 0; j < floatSamplesPerFrame.size(); j++) {
 
             ae.setFloatBuffer(floatSamplesPerFrame.get(j));//metto nel buffer di ae un blocco di campioni alla volta (singoli frame)
             mfcc.process(ae);//calcolo mfcc sul singolo frame
@@ -152,181 +158,23 @@ public class SpeakerRecog extends AsyncTask<String,Void,String> {
 
         }
 
-        deltadelta = computeDeltas(computeDeltas(cepCoeffPerFrame,2),2);//calcolo i delta di secondo ordine applicando due volte la funzione delta
+        deltadelta = computeDeltas(computeDeltas(cepCoeffPerFrame, 2), 2);//calcolo i delta di secondo ordine applicando due volte la funzione delta
 
+        printFeaturesOnFileFormat(cepCoeffPerFrame, deltadelta, path + "/rec" + strings[1] + ".txt", 1);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //print features on file
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        int numberOfTrainingSpeakers = 2;
-        int totalNumberOfFeatures = 2 * (cepCoeffPerFrame.get(0).length);
-        int numberOfFramesPerSpeaker = cepCoeffPerFrame.size();
-
-
-
-                /**/
-
-
-                try{
-
-                //printFeaturesOnFileFormat(cepCoeffPerFrame,deltadelta, storeDir + "/testDataFormat" + numberOfTest + ".txt",speaker);
-
-
-                ArrayList<double[]> union = uniteAllFeaturesInOneList(cepCoeffPerFrame, deltadelta);//converto i dati di test in un array di svm_node
-                svm_node[][] testData = new svm_node[numberOfFramesPerSpeaker][totalNumberOfFeatures + 1];
-                svm_node[][] scaledTestData = new svm_node[numberOfFramesPerSpeaker][totalNumberOfFeatures +1];
-
-                for (int i = 0; i < numberOfFramesPerSpeaker; i++) {
-
-                    for (int j = 0; j < totalNumberOfFeatures; j++) {
-
-                        svm_node node = new svm_node();
-                        node.index = j;
-                        node.value = union.get(i)[j];
-
-                        testData[i][j] = node;
-                    }
-
-                    svm_node finalNode = new svm_node();
-                    finalNode.index = -1;
-                    finalNode.value = 0;
-
-                    testData[i][totalNumberOfFeatures] = finalNode;
-                }
-
-                //testData = readTestDataFromFormatFile(storeDir + "/testDataFormatMJ1" + numberOfTest + ".txt",numberOfFramesPerSpeaker,totalNumberOfFeatures);
-
-                //testData = readTestDataFromFormatFile(storeDir + "/Jacopo21.txt",numberOfFramesPerSpeaker,totalNumberOfFeatures);
-
-                testData = readTestDataFromFormatFile(path + "/CC1.txt",numberOfFramesPerSpeaker,totalNumberOfFeatures);
-
-                ArrayList<ArrayList<Double>> resultsList = new ArrayList<>();
-
-                double res;
-                svm_model loadedModel;
-
-                for(int j = 0; j< numberOfTrainingSpeakers; j++) {
-
-                    int speaker = j + 1;
-                    resultsList.add(j,new ArrayList<Double>());
-
-                    loadedModel = svm.svm_load_model(new BufferedReader(new FileReader(path + "/modelMultiSpeaker" + String.valueOf(speaker) + ".txt")));
-                    matlabModelToAndroidModel(loadedModel);
-
-                    scaledTestData = scaleTestData(testData,speaker,path,1);
-
-                    for (int i = 0; i < numberOfFramesPerSpeaker; i++) {
-
-                        res = svm.svm_predict(loadedModel,scaledTestData[i]);
-                        resultsList.get(j).add(i, res);
-                    }
-                }
-
-
-                int speaker = - 1;
-                int frequencyForList = 0;
-
-                ArrayList<Integer> frequencies = new ArrayList<>();
-
-                for(int i = 0; i< resultsList.size(); i++){
-
-                    frequencyForList = Collections.frequency(resultsList.get(i), new Double(1));
-
-                    frequencies.add(i,frequencyForList);
-
-                }
-
-                ArrayList<String> names = new ArrayList<>();
-                names.add(0, "Speaker One MB");
-                names.add(1,"Speaker Two MJ");
-                names.add(2,"Speaker Three MT");
-
-                ArrayList<Double> percentages = new ArrayList<>();
-                percentages.add(0,0.8);
-                percentages.add(1,0.8);
-                percentages.add(2,0.9);
-
-
-                ArrayList<Double> relativeFrequencies = new ArrayList<>();
-                ArrayList<Double> relativeFrequenciesCopy = new ArrayList<>();
-
-                for(int i=0; i< numberOfTrainingSpeakers; i++)
-                {
-                    relativeFrequencies.add(i,frequencies.get(i) - percentages.get(i)*numberOfFramesPerSpeaker);
-                    relativeFrequenciesCopy.add(i,frequencies.get(i)- percentages.get(i)*numberOfFramesPerSpeaker);
-                }
-
-                Collections.sort(relativeFrequenciesCopy,Collections.<Double>reverseOrder());
-
-                /**/
-
-                String recognizedSpeaker = "Unknown" + ", not speaker" + String.valueOf(relativeFrequencies.indexOf(relativeFrequenciesCopy.get(0)) + 1) + " for " + String.valueOf(-relativeFrequenciesCopy.get(0)) + " frames in multi";
-
-                int recogSpeaker = -1;
-
-                for(int j = 0; j< numberOfTrainingSpeakers; j++)
-                {
-                    speaker = relativeFrequencies.indexOf(relativeFrequenciesCopy.get(j)) + 1;
-
-                    if(frequencies.get(j) > percentages.get(speaker -1)*numberOfFramesPerSpeaker)
-                    {
-                        //recognizedSpeaker = names.get(speaker - 1) + " for " + String.valueOf(relativeFrequenciesCopy.get(j)) + " frames";
-                        recogSpeaker = j + 1;
-                        break;
-                    }
-
-                }
-
-
-                if(recogSpeaker != -1)
-                {
-                    svm_model oneClassModel = svm.svm_load_model(path + "/modelOneSpeaker" + String.valueOf(recogSpeaker) + ".txt");
-                    matlabModelToAndroidModel(oneClassModel);
-
-                    double predictedLabel = 0;
-                    int count = 0;
-
-                    ArrayList<Double> oneClassThr = new ArrayList<>();
-                    oneClassThr.add(0, 0.05);
-                    oneClassThr.add(1, 0.08);
-
-
-                    scaledTestData = scaleTestData(testData,recogSpeaker,path,0);
-
-                    for (int i = 0; i < numberOfFramesPerSpeaker; i++) {
-                        predictedLabel = svm.svm_predict(oneClassModel, scaledTestData[i]);
-                        if (predictedLabel == 1) {
-                            count++;
-                        }
-                    }
-
-                    if (((double) count) / numberOfFramesPerSpeaker >= oneClassThr.get(recogSpeaker - 1)) {
-                        recognizedSpeaker = names.get(recogSpeaker-1) + "for " + String.valueOf(relativeFrequencies.get(recogSpeaker-1) + " frames in multi and " + String.valueOf(count-oneClassThr.get(recogSpeaker-1)*numberOfFramesPerSpeaker) + " frames in oneC");
-                    }
-                    else
-                    {
-                        recognizedSpeaker = "Unknown, not speaker " + String.valueOf(recogSpeaker) + " for " + String.valueOf(oneClassThr.get(recogSpeaker-1)*numberOfFramesPerSpeaker-count) + " frames in oneC";
-                    }
-                }
-
-                return recognizedSpeaker;
-
-
-            }
-            catch (IOException exception) {
-                Log.e("Speaker Recognition", "Error while loading models");
-                return null;
-            }
+    return doToast;
 
     }
 
-
-
     @Override
-    protected void onPostExecute(String recognizedSpeaker) {
-        super.onPostExecute(recognizedSpeaker);
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
 
-        //Toast.makeText(context,recognizedSpeaker,Toast.LENGTH_LONG).show();
-        textView.setText(recognizedSpeaker);
+        if(result)
+        {
+        Toast.makeText(context,"Ended conversion",Toast.LENGTH_LONG).show();
+        }
     }
 }
